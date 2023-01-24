1b9bfaaa5ac92a680f8ff469f60f410ab6c21199
Asmaa Mohammed <Asmaa@ocean-kw.local>
2023-01-24 17:42:48 +0200

    express and server definition

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('');
+import express from 'express';
+//application ==> عشان احنا بنستخدم ال typescript
+//وممكن ما نستخدمهوش
+//, { application }
+
+//create instance of server
+const app = express();
+const port = 3000;
+
+console.log('hello·world');
+
+//start express server
+//ممكن نقوله ابعتلي callback فانكشن بيحصلها لما السيرفر يشتغل
+app.listen(port, () => {
+  console.log(`server is run at port ${port}`);
+});
+
+export default app;
